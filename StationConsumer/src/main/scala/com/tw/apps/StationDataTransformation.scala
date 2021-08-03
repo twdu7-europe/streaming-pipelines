79c73c70eeac123c2456e2b9bd85e68c2460a805
Arne Laponin <arne.laponin@thoughtworks.com>
2021-08-03 18:08:40 +0200

    WiP Apply transformation for Marseille data in the consumer

--- StationConsumer/src/main/scala/com/tw/apps/StationDataTransformation.scala
+++ StationConsumer/src/main/scala/com/tw/apps/StationDataTransformation.scala
@@ -18,6 +18,12 @@ object StationDataTransformation {
     extractSFStationStatus(payload)
   }
 
+  val marseilleToStationStatus: String => Seq[StationData] = raw_payload => {
+    val json = JSON.parseFull(raw_payload)
+    val payload = json.get.asInstanceOf[Map[String, Any]]("payload")
+    extractMarseilleStationStatus(payload)
+  }
+
   private def extractSFStationStatus(payload: Any) = {
 
     val network: Any = payload.asInstanceOf[Map[String, Any]]("network")
@@ -40,6 +46,28 @@ object StationDataTransformation {
       })
   }
 
+  private def extractMarseilleStationStatus(payload: Any) = {
+
+    val network: Any = payload.asInstanceOf[Map[String, Any]]("network")
+
+    val stations: Any = network.asInstanceOf[Map[String, Any]]("stations")
+
+    stations.asInstanceOf[Seq[Map[String, Any]]]
+      .map(x => {
+        StationData(
+          x("free_bikes").asInstanceOf[Double].toInt,
+          x("empty_slots").asInstanceOf[Double].toInt,
+          true,
+          true,
+          Instant.from(DateTimeFormatter.ISO_INSTANT.parse(x("timestamp").asInstanceOf[String])).getEpochSecond,
+          x("id").asInstanceOf[String],
+          x("name").asInstanceOf[String],
+          x("latitude").asInstanceOf[Double],
+          x("longitude").asInstanceOf[Double]
+        )
+      })
+  }
+
   def sfStationStatusJson2DF(jsonDF: DataFrame, spark: SparkSession): DataFrame = {
     val toStatusFn: UserDefinedFunction = udf(sfToStationStatus)
 
@@ -53,6 +81,15 @@ object StationDataTransformation {
     import spark.implicits._
 
     jsonDF.select(from_json($"raw_payload", ScalaReflection.schemaFor[StationData].dataType) as "status")
+      .select($"status.*")
+  }
+
+  def marsStationStatusJson2DF(jsonDF: DataFrame, spark: SparkSession): DataFrame = {
+    val toStatusFn: UserDefinedFunction = udf(marseilleToStationStatus)
+
+    import spark.implicits._
+
+    jsonDF.select(explode(toStatusFn(jsonDF("raw_payload"))) as "status")
       .select($"status.*")
   }
 }
--- StationConsumer/src/test/scala/com/tw/apps/StationDataTransformationTest.scala
+++ StationConsumer/src/test/scala/com/tw/apps/StationDataTransformationTest.scala
@@ -1,7 +1,7 @@
 package com.tw.apps
 
-import StationDataTransformation.nycStationStatusJson2DF
-import org.apache.spark.sql.SparkSession
+import StationDataTransformation.{marsStationStatusJson2DF, nycStationStatusJson2DF}
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
 import org.apache.spark.sql.catalyst.ScalaReflection
 import org.scalatest._
 
@@ -65,6 +65,101 @@ class StationDataTransformationTest extends FeatureSpec with Matchers with Given
       row1.get(6) should be("Atlantic Ave & Fort Greene Pl")
       row1.get(7) should be(40.68382604)
       row1.get(8) should be(-73.97632328)
+    }
+
+    scenario("Transform marseille station data") {
+          val testDataMarseille =
+                """"network": {
+            "company": [
+              "JCDecaux"
+            ],
+            "gbfs_href": "https://gbfs.fordgobike.com/gbfs/gbfs.json",
+            "href": "/v2/networks/le-velo",
+            "id": "le-velo",
+            "location": {
+                  "city": "Marseille",
+                  "country": "FR",
+                  "latitude": 43.296482,
+                  "longitude": 5.36978
+                },
+            "name": "Le vélo",
+            "source": "https://developer.jcdecaux.com",
+            "stations": [
+              {
+                    "empty_slots": 14,
+                    "extra": {
+                      "address": "391 MICHELET - 391 BOULEVARD MICHELET",
+                      "banking": true,
+                      "bonus": false,
+                      "last_update": 1628003594000,
+                      "slots": 19,
+                      "status": "OPEN",
+                      "uid": 8149
+                    },
+                    "free_bikes": 5,
+                    "id": "686e48654a218c70daf950a4e893e5b0",
+                    "latitude": 43.25402727813068,
+                    "longitude": 5.401873594694653,
+                    "name": "8149-391 MICHELET",
+                    "timestamp": "2021-08-03T15:18:38.421000Z"
+                  },
+                  {
+                    "empty_slots": 10,
+                    "extra": {
+                      "address": "TEISSEIRE ROUBAUD - ANGLE RUE RAYMOND TEISSEIRE ET RUE MAGUY ROUBAUD",
+                      "banking": true,
+                      "bonus": false,
+                      "last_update": 1628003798000,
+                      "slots": 10,
+                      "status": "OPEN",
+                      "uid": 9207
+                    },
+                    "free_bikes": 0,
+                    "id": "259c5e65f9e2af98046069144c666aa6",
+                    "latitude": 43.27252367086013,
+                    "longitude": 5.399686062414487,
+                    "name": "9207- TEISSEIRE - ROUBAUD",
+                    "timestamp": "2021-08-03T15:18:38.428000Z"
+                  },
+            ]
+          }"""
+
+          Given("Sample data for status_information")
+          val testDF1 = Seq(testDataMarseille).toDF("raw_payload")
+
+          When("Transformations are applied")
+          val resultDF1 = testDF1.transform(marsStationStatusJson2DF(_, spark))
+
+          Then("Useful columns are extracted")
+          resultDF1.schema.fields(0).name should be("bikes_available")
+          resultDF1.schema.fields(0).dataType.typeName should be("integer")
+          resultDF1.schema.fields(1).name should be("docks_available")
+          resultDF1.schema.fields(1).dataType.typeName should be("integer")
+          resultDF1.schema.fields(2).name should be("is_renting")
+          resultDF1.schema.fields(2).dataType.typeName should be("boolean")
+          resultDF1.schema.fields(3).name should be("is_returning")
+          resultDF1.schema.fields(3).dataType.typeName should be("boolean")
+          resultDF1.schema.fields(4).name should be("last_updated")
+          resultDF1.schema.fields(4).dataType.typeName should be("long")
+          resultDF1.schema.fields(5).name should be("station_id")
+          resultDF1.schema.fields(5).dataType.typeName should be("string")
+          resultDF1.schema.fields(6).name should be("name")
+          resultDF1.schema.fields(6).dataType.typeName should be("string")
+          resultDF1.schema.fields(7).name should be("latitude")
+          resultDF1.schema.fields(7).dataType.typeName should be("double")
+          resultDF1.schema.fields(8).name should be("longitude")
+          resultDF1.schema.fields(8).dataType.typeName should be("double")
+
+          val row1 = resultDF1.head()
+          row1.get(0) should be(5)
+          row1.get(1) should be(14)
+          row1.get(2) shouldBe true
+          row1.get(3) shouldBe true
+          row1.get(4) should be(1536242527)
+          row1.get(5) should be("le-velo")
+          row1.get(6) should be("Le vélo")
+          row1.get(7) should be(43.25402727813068)
+          row1.get(8) should be(5.401873594694653)
     }
   }
 }
