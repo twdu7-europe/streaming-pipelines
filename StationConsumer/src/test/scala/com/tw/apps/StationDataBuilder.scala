e111386cd3ca76b7824086854a7688fcde51b3f8
Mario Giampietri <mgiampie@thoughtworks.com>
2021-08-04 16:48:08 +0100

    Station data validation - Added validation for docks available and duplicate station ids

--- StationConsumer/src/test/scala/com/tw/apps/StationDataBuilder.scala
+++ StationConsumer/src/test/scala/com/tw/apps/StationDataBuilder.scala
@@ -1,12 +1,12 @@
 package com.tw.apps
 
 object StationDataBuilder {
-  def buildStationRow(stationId: String, latitude: String = "40.68382604", longitude: String = "-73.97632328", bikesAvailable: Int = 19): String = {
+  def buildStationRow(stationId: String, latitude: String = "40.68382604", longitude: String = "-73.97632328", bikesAvailable: Int = 19, docksAvailable: Int = 41): String = {
     s"""
        {
           "station_id":"${stationId}",
           "bikes_available":${bikesAvailable},
-          "docks_available":41,
+          "docks_available":${docksAvailable},
           "is_renting":true,
           "is_returning":true,
           "last_updated":1536242527,
