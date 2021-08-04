f00fef536a39ae95660b517164276ec5d72ca779
Kat Hicks <kathicks@thoughtworks.com>
2021-08-04 15:36:34 +0100

    Add additional failing test for validating latitude

---
+++ StationConsumer/src/test/scala/com/tw/apps/StationDataBuilder.scala
@@ -0,0 +1,19 @@
+package com.tw.apps
+
+object StationDataBuilder {
+  def buildStationRow(stationId: Int, latitude: String = "40.68382604", longitude: String = "-73.97632328"): String = {
+    s"""
+       {
+          "station_id":"${stationId}",
+          "bikes_available":19,
+          "docks_available":41,
+          "is_renting":true,
+          "is_returning":true,
+          "last_updated":1536242527,
+          "name":"Atlantic Ave & Fort Greene Pl",
+          "latitude":${latitude},
+          "longitude":${longitude}
+       }
+    """
+  }
+}
--- StationConsumer/src/test/scala/com/tw/apps/StationDataValidatorTest.scala
+++ StationConsumer/src/test/scala/com/tw/apps/StationDataValidatorTest.scala
@@ -1,57 +1,33 @@
 package com.tw.apps
 
+import com.tw.apps.StationDataBuilder.buildStationRow
 import com.tw.apps.StationDataTransformation.nycStationStatusJson2DF
 import com.tw.apps.StationDataValidator._
 import org.apache.spark.sql.{Row, SparkSession}
 import org.scalatest._
 
-class StationDataValidatorTest extends FeatureSpec with Matchers with GivenWhenThen {
+//      DONE 1. Check in the file that there is a long
+//      1.2     and lat for each station
+//      2. Confirm that each station id is listed only once.
+//      3. Bikes Available should be a non-negaitve number
+//      4. Docks Available should be a non-negaitve number
 
+class StationDataValidatorTest extends FeatureSpec with Matchers with GivenWhenThen {
   feature("Apply station status validation to data frame") {
     val spark = SparkSession.builder.appName("Test App").master("local").getOrCreate()
     import spark.implicits._
 
-    scenario("Validate nyc station data frame") {
-
-      val testStationData = Seq(
-        """{
-          "station_id":"83",
-          "bikes_available":19,
-          "docks_available":41,
-          "is_renting":true,
-          "is_returning":true,
-          "last_updated":1536242527,
-          "name":"Atlantic Ave & Fort Greene Pl",
-          "latitude":40.68382604,
-          "longitude":-73.97632328
-          }""",
-        """{
-          "station_id":"84",
-          "bikes_available":19,
-          "docks_available":41,
-          "is_renting":true,
-          "is_returning":true,
-          "last_updated":1536242527,
-          "name":"Atlantic Ave & Fort Greene Pl",
-          "latitude":40.68382604,
-          "longitude":null
-          }""",
-        """{
-          "station_id":"85",
-          "bikes_available":19,
-          "docks_available":41,
-          "is_renting":true,
-          "is_returning":true,
-          "last_updated":1536242527,
-          "name":"Atlantic Ave & Fort Greene Pl",
-          "latitude":40.68382604
-          }""")
+    scenario("Validating missing longitude") {
+      val testStationDataWithMissingLongitude = Seq(
+        buildStationRow(1),
+        buildStationRow(2, longitude = "null")
+      )
 
       val expectedResult =
-        Array(Row.fromSeq(Seq (19, 41, true, true, 1536242527, 83, "Atlantic Ave & Fort Greene Pl", 40.68382604, -73.97632328)))
+        Array(Row.fromSeq(Seq(19, 41, true, true, 1536242527, 83, "Atlantic Ave & Fort Greene Pl", 40.68382604, -73.97632328)))
 
-      Given("Sample data for station_status")
-      val testDF = testStationData.toDF("raw_payload")
+      Given("Some rows are missing longitude")
+      val testDF = testStationDataWithMissingLongitude.toDF("raw_payload")
 
       val transformedDF = testDF.transform(nycStationStatusJson2DF(_, spark))
 
@@ -62,13 +38,31 @@ class StationDataValidatorTest extends FeatureSpec with Matchers with GivenWhenT
       val filteredResult = filterValidData(transformedDF)
 
       Then("Only valid rows are retrieved")
-      filteredResult equals expectedResult
+      assert(filteredResult sameElements expectedResult)
+    }
 
-//      DONE 1. Check in the file that there is a long
-//      1.2     and lat for each station
-//      2. Confirm that each station id is listed only once.
-//      3. Bikes Available should be a non-negaitve number
-//      4. Docks Available should be a non-negaitve number
+    scenario("Validating missing latitude") {
+      val testStationDataWithMissingLatitude = Seq(
+        buildStationRow(1),
+        buildStationRow(2, latitude = "null")
+      )
+
+      val expectedResult =
+        Array(Row.fromSeq(Seq(19, 41, true, true, 1536242527, 1, "Atlantic Ave & Fort Greene Pl", 40.68382604, -73.97632328)))
+
+      Given("Some rows are missing latitude")
+      val testDF = testStationDataWithMissingLatitude.toDF("raw_payload")
+
+      val transformedDF = testDF.transform(nycStationStatusJson2DF(_, spark))
+
+      print("DISPLAYING TRANSFORMED JSON DATA ***************************************************************")
+
+      transformedDF.show()
+      When("Validations are applied")
+      val filteredResult = filterValidData(transformedDF)
+
+      Then("Only valid rows are retrieved")
+      assert(filteredResult sameElements expectedResult)
     }
   }
 }
