fe10a03d0b8916884c198bb5af5ca8169cb44cd4
jmolina91 <javier.m@thoughtworks.com>
2021-08-11 16:20:47 +0200

    Javi/Mario: wip

--- airflow/dags/test_marseille_producer.py
+++ airflow/dags/test_marseille_producer.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.operators.bash import BashOperator
 
 default_args = {
     'owner': 'airflow',
@@ -7,6 +8,13 @@ default_args = {
 
 dag = DAG('test_marseille_producer', default_args=default_args)
 
+dummy = BashOperator(
+    task_id='print_date',
+    bash_command='date',
+)
+
 submit_job = SparkSubmitOperator(
     application="/opt/airflow/spark_apps/test_producer_spark_job.py", task_id="submit_job"
 )
+
+dummy >> submit_job
